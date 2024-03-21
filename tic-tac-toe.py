2a1cef7b7461c73f820143f09b12961eaf2110a9
Anna Floyd <annafloyd12@gmail.com>
2024-03-21 12:49:25 -0500

    Created board and way to display board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,18 @@
+board = [['_', '_', '_'],
+         ['_', '_', '_'],
+         ['_', '_', '_']]
+
 def main():
     print("Main method.")
+    displayBoard()
+
+def displayBoard():
+    for x in range(len(board)):
+        output = "|"
+        for symbol in board[x]:
+            output += symbol + "|"
+        print(output)
+
 
 if __name__ == "__main__":
     main()
